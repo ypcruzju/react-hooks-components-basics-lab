5687394910a93183025c0145db6cff8926cabde1
yaxpaccruzjuarez <ypjuarez11@gmail.com>
2022-05-18 13:40:55 -0400

    commit tree 6ba379dd5599609caf378e704237ddf4395d3760

--- src/components/App.js
+++ src/components/App.js
@@ -16,13 +16,33 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About (){
+  return (  
+    <div id="about">
+      <h2>
+        "About"
+      </h2>
+    
+
+    </div>
+
+
+
+
+  )
+  
+  /* write an <About> component here */
+
+
+}
 
 
 function App() {
   return (
     <div>
-      <NavBar />
+      {<NavBar/>}
+      {<Home />}
+      {<About/>}
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
